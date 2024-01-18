2896c71bc3f070ee6a89de22d745c34ef163ccea
Pablo Ribeiro <pabloribeiro2302@gmail.com>
2024-01-18 11:48:45 -0300

    update

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -6,13 +6,13 @@ export default function Section2(){
             <div className="section2--imgContainer">
                 <a img className="img-large img-cafe-desk" href="https://pabloribeiro23.github.io/blogCafe-udemy/" target="_blank"><img className="img-large img-cafe-desk" src="./images/blogcafe.png" alt="" /></a>
                 <a img className="img-mobile img-cafe-mobile" href="https://pabloribeiro23.github.io/blogCafe-udemy/" target="_blank"><img className="img-mobile img-cafe-mobile" src="./images/mobile-cafe.png" alt="" /></a>
-                <a img className="img-mobile img-todo-mobile" target="_blank" href="https://pabloribeiro23.github.io/Gestor-App/"><img className="img-mobile img-todo-mobile" src="./images/mobile-todo.png" alt="" /></a>
                 <a img className="img-mobile img-ip-mobile" target="_blank" href="https://pabloribeiro23.github.io/IP-Tracker/"><img className="img-mobile img-ip-mobile" src="./images/mobile-ip.png" alt="" /></a>
                 <a img className="img-mobile img-form-mobile" target="_blank" href="https://pabloribeiro23.github.io/multiStep-form/"><img className="img-mobile img-form-mobile" src="./images/mobile-form.png" alt="" /></a>
                 <a img className="img-large img-form-desk" target="_blank" href="https://pabloribeiro23.github.io/multiStep-form/"><img className="img-large img-form-desk" src="./images/form.png" alt="" /></a>
                 <a img className="img-large img-gestor" target="_blank" href="https://gestorpresupuestoreact.netlify.app/"><img className="img-large img-form-desk" src="./images/gastoGestor.png" alt="" /></a>
                 <a img className="img-mobile img-tictac" target="_blank" href="https://tictactoeaf.netlify.app"><img className="img-large img-form-desk" src="./images/mobile.png" alt="" /></a>
                 <a img className="img-large img-pacientesVet" target="_blank" href="https://gestorpacientesvet.netlify.app"><img className="img-large img-form-desk" src="./images/pacientesVet.png" alt="" /></a>
+                <a img className="img-large img-pizza" target="_blank" href="https://pabloribeiro23.github.io/coursera-htmlCssAssignment/"><img className="img-large img-form-desk" src="./images/pizza.png" alt="" /></a>
             </div>
             </div>
         </section>
